c1c30ab4e704776168e0269efd2e5ccfbb3b3bd2
Pablo Albuquerque <pablo.limaalb@gmail.com>
2025-08-06 22:23:27 -0300

    Database Connect

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,6 +2,7 @@ import {z} from "zod";
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.string().url().startsWith("postgresql://"),
 });
 
 export const env = envSchema.parse({
