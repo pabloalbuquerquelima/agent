cff51ea0b87820ab8005c8c49c431470d8035a2d
Pablo Albuquerque <pablo.limaalb@gmail.com>
2025-08-07 18:45:40 -0300

    add filter return rooms and add env info

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,7 +1,16 @@
-import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { db } from "../../db/connection.ts";
+import { schema } from "./../../db/schema/index.ts";
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
-  app.get("/rooms",() => {
-   return "hello world";
+  app.get("/rooms", async () => {
+    const results = await db
+      .select({
+        id: schema.rooms.id,
+        name: schema.rooms.name,
+      })
+      .from(schema.rooms)
+      .orderBy(schema.rooms.created_at);
+    return results;
   });
 };
